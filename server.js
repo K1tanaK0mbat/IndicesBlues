9a4748a5978bf59d154dc35fec354d908705a651
K1tanaK0mbat <kkeayouth@gmail.com>
2023-10-09 12:18:16 -0500

    tried debgging

--- server.js
+++ server.js
@@ -22,3 +22,8 @@ app.get('/notes', (req, res) =>
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
